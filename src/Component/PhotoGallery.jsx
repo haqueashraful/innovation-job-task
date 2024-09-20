dd0aba354e5dbb3ca9c7f9c47d55a045420f28df
haqueashraful <123ashrafulhaque@gmail.com>
2024-09-20 17:13:55 +0600

    new section

---
+++ src/Component/PhotoGallery.jsx
@@ -0,0 +1,23 @@
+
+const PhotoGallery = () => {
+    return (
+        <div className=" flex justify-between items-center">
+            {/* photo */}
+            <div>
+
+            </div>
+
+            {/* text */}
+
+            <div className=" w-full items-center">
+            <p className=" text-2xl text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore e.</p>
+
+            <button className="px-4  py-2 border-2 rounded-full border-blue-400 bg-white text-blue-400  hover:bg-blue-400 hover:text-white items-center">
+                Sign up
+            </button>
+            </div>
+        </div>
+    );
+};
+
+export default PhotoGallery;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import MainBanner from "../Component/MainBanner"
+import MainBanner from "../Component/MainBanner";
+import PhotoGallery from "@/Component/PhotoGallery";
 export default function Home() {
   return (
     <>
-    <MainBanner />
+      <MainBanner />
 
-    {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <PhotoGallery />
+      {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -100,7 +102,6 @@ export default function Home() {
         </a>
       </footer>
     </div> */}
-
     </>
   );
 }
